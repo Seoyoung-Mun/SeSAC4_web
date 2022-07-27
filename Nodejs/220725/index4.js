5ef1db66a706a8a77246f6c10c3b0649d02437ba
Seoyoung-Mun <tjdud330@gmail.com>
2022-07-27 17:44:48 +0900

    login연습

--- Nodejs/220725/index4.js
+++ Nodejs/220725/index4.js
@@ -23,7 +23,8 @@ app.post("/receive2", function(req,res){
     console.log("receive-post");
     console.log( req.body );
     res.render("receive4", req.body);
-    
+    // 1. info.txt의 내용을 //로 구분하여 배열에 담는다.
+// 2. res에 받아온 값과 배열의 첫번째 값과 마지막 값을 비교하는 조건문을 작성한다.
     const i = req.body.name+'//'+req.body.ID+'//'+req.body.password;
     console.log(i);
 
--- Nodejs/220725/index5.js
+++ Nodejs/220725/index5.js
@@ -1,8 +1,9 @@
-//회원가입
+//로그인
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = 8000;
 const bodyParser = require("body-parser");
+const fs = require("fs");
 
 app.set("view engine","ejs");
 app.use( express.static( "public" ) );
@@ -10,7 +11,7 @@ app.use( express.urlencoded({extended: true }));
 app.use( bodyParser.json() );
 
 app.get("/", function(req,res){
-    res.render("index4");
+    res.render("index5");
 });
 
 app.get("/receive", function(req,res){
@@ -22,9 +23,20 @@ app.get("/receive", function(req,res){
 app.post("/receive2", function(req,res){ 
     console.log("receive-post");
     console.log( req.body );
-    res.render("receive5", req.body);
+    // 1. info.txt의 내용을 //로 구분하여 배열에 담는다.
+    let string = fs.readFileSync("./info.txt").toString();
+    console.log(string);
+    let strArray = string.split('//');
+    console.log(strArray[0],strArray[1],strArray[2]);
     
+    // 2. res에 받아온 값과 배열1값과 배열2 값을 비교하는 조건문을 작성한다.
+    let ID = req.body.ID
+    let password = req.body.password
+    if ( strArray[1] == ID && strArray[2] == password){
+        res.render("receive5-1");
+    } else  res.render("receive5-2");
 });
+
 
 app.listen(port,()=>{
     console.log("Server Port", port );

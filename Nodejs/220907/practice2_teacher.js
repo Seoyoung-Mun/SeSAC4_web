e891f04b8aeb44587b2a94618fc24994e7cc6831
Seoyoung-Mun <tjdud330@gmail.com>
2022-09-15 17:54:09 +0900

    review socket

--- Nodejs/220907/practice2_teacher.js
+++ Nodejs/220907/practice2_teacher.js
@@ -10,6 +10,7 @@ app.get("/", function(req,res){
 // io -> 클라이언트와의 모든 연결을 갖고있음.
 // socket -> 클라이언트 하나 . socket.id 클라이언트를 구분하는 식별자
 var list = {};
+// list = {k3434gfg: "seo"}
 io.on("connection", function(socket){
     console.log("connected", socket.id);
     socket.on("info2", function(data){
@@ -20,14 +21,17 @@ io.on("connection", function(socket){
     // io.emit("notice", socket.id + "님이 입장하셨습니다.");
     // socket.emit("info", socket.id);
     socket.on("send", function(data){
-        // 넘어온 data = { id :~~~ , msg :~~~}
+        // 넘어온 data = { msg :~~~ , to :~~~}
         console.log("client messge : ", data.msg );
+        // data = {mag :~~~ , to :~~~, is_dm : false, nickname : ~~~}
         data["is_dm"] = false;
         data["nickname"] = list[socket.id];
         if ( data.to == "전체"){
             io.emit("newMessage", data);
         } else {
             data["is_dm"] = true;
+            // list = {socket.id: "nickname"}
+            // list[socket.id] = "nickname"
             let socketID = Object.keys(list).find( (key) => { return list[key] === data.to; } );
             io.to(socketID).emit("newMessage", data);
             socket.emit("newMessage", data);

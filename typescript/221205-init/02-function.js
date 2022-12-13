f05cbbb98353176b27dc90b0ff465f2eb4766798
Seoyoung-Mun <tjdud330@gmail.com>
2022-12-13 16:31:35 +0900

    tsc

--- typescript/221205-init/02-function.js
+++ typescript/221205-init/02-function.js
@@ -1,20 +1,43 @@
 // // function 함수이름 (변수명:타입) : 반환타입 {}
 // // 
-var testFunction = function () {
-    return 'test';
+// const testFunction = ():string => {
+//     return 'test';
+// }
+// console.log (testFunction ())
+function sum1(a, b) {
+    return a + b;
+}
+console.log(sum1(1, 'a'));
+console.log(sum1(1, null)); // 함수에 인자를 안보내면 안됨
+function sum2(a, b) {
+    return a + b;
+}
+console.log(sum2(1, 2));
+//선택적으로 인자를 받아야 할 때는 ? 사용
+function sum3(a, b) {
+    return a + b;
+}
+console.log("sum3 : ", sum3(1));
+var person = { name: '코딩온', age: 10 };
+function getInfo(obj) {
+}
+function getInfo2(obj) {
+}
+var person2 = { name: '코딩온', age: 10 };
+person2.age = person2.age + 1;
+;
+var loginUser = function (id, pw) {
+    return true;
 };
-console.log(testFunction());
-// function sum1 (a,b) {
-//     return a + b;
-// }
-// console.log( sum1(1,'a') );
-// console.log( sum1(1 ,null)); // 함수에 인자를 안보내면 안됨
-// function sum2 ( a :number, b:number ) : number {
-//     return a + b;
-// }
-// console.log( sum2(1,2) );
-// //선택적으로 인자를 받아야 할 때는  ? 사용
-// function sum3 ( a :number, b?:number ) : number {
-//     return a + b;
-// }
-// console.log( "sum3", sum3(1 , un) );
+var MyBook = /** @class */ (function () {
+    function MyBook() {
+        this.title = '책';
+        this.date = "2022-12-05";
+    }
+    MyBook.prototype.getInfo = function () {
+        console.log("정보");
+    };
+    MyBook.prototype.changeTitle = function (newTitle) {
+    };
+    return MyBook;
+}());
